4f8d5c4705107ed7942c5c6803b8f140bdbe8ebe
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-03-05 18:10:06 +0100

    Add preview entity construction helper

---
+++ src/main/java/org/fentanylsolutions/vintagedamageindicators/client/PreviewEntityFactory.java
@@ -0,0 +1,47 @@
+package org.fentanylsolutions.vintagedamageindicators.client;
+
+import java.lang.reflect.Constructor;
+
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
+
+import org.fentanylsolutions.vintagedamageindicators.VintageDamageIndicators;
+
+public final class PreviewEntityFactory {
+
+    private PreviewEntityFactory() {}
+
+    public static EntityLivingBase create(String registryName, World world, String context) {
+        Object classMapping = EntityList.stringToClassMapping.get(registryName);
+        if (!(classMapping instanceof Class<?>)) {
+            VintageDamageIndicators.LOG
+                .warn("Failed to construct preview entity '{}' in {}: no class mapping found.", registryName, context);
+            return null;
+        }
+
+        Class<?> entityClass = (Class<?>) classMapping;
+        try {
+            Constructor<?> constructor = entityClass.getConstructor(World.class);
+            Object created = constructor.newInstance(world);
+            if (created instanceof EntityLivingBase) {
+                return (EntityLivingBase) created;
+            }
+
+            VintageDamageIndicators.LOG.warn(
+                "Failed to construct preview entity '{}' ({}) in {}: entity is not living.",
+                registryName,
+                entityClass.getName(),
+                context);
+            return null;
+        } catch (Throwable throwable) {
+            VintageDamageIndicators.LOG.warn(
+                "Failed to construct preview entity '{}' ({}) in {}.",
+                registryName,
+                entityClass.getName(),
+                context,
+                throwable);
+            return null;
+        }
+    }
+}
